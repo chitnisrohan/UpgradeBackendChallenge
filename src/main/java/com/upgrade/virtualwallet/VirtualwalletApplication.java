0b539d4a3f673d3e0027f31c6a7a5f7d4129e581
Rohan Chitnis <rohanchitnis@Rohans-MacBook.local>
2018-10-17 15:39:11 -0400

    Started with project. created User entity to test H2 DB

---
+++ src/main/java/com/upgrade/virtualwallet/VirtualwalletApplication.java
@@ -0,0 +1,12 @@
+package com.upgrade.virtualwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualwalletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirtualwalletApplication.class, args);
+    }
+}
---
+++ src/main/java/com/upgrade/virtualwallet/models/User.java
@@ -0,0 +1,67 @@
+package com.upgrade.virtualwallet.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String phone;
+
+    protected User() {}
+
+    public User(String firstName, String lastName, String address, String phone) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
